af18985129b02636efb286bdd41fad5b24cb84a4
allomhg <allomhg@gmail.com>
2024-02-17 12:36:06 +0000

    Fleshing out

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <main>
-      <header>
-        <h1 className="text-3xl font-bold">1 Repetition Max Calculator (1RM)</h1>
+    <main className="container rounded">
+      <header className="bg-black p-4 rounded-t text-center">
+        <h1 className="text-3xl font-bold">One Repetition Max Calculator</h1>
       </header>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <h2 className="text-2xl p-4">One Rep Max (1RM)</h2>
+        <p className="px-4">One repetition max is the maximum amount of weight you can push for one repetition for a given exercise.</p>
+      </section>
     </main>
   )
 }
