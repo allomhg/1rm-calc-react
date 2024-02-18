ed5b7979a0863e8656675154bc21512c729a18d5
allomhg <allomhg@gmail.com>
2024-02-18 09:32:06 +0000

    Breaking out Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 // import { useState } from 'react'
 import './App.css'
+import { Header } from './components/header/Header'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <main className="container rounded">
-      <header className="bg-black p-4 rounded-t text-center">
-        <h1 className="text-3xl font-bold">One Repetition Max Calculator</h1>
-      </header>
-      <section>
-        <h2 className="text-2xl p-4">One Rep Max (1RM)</h2>
-        <p className="px-4">One repetition max is the maximum amount of weight you can push for one repetition for a given exercise.</p>
-      </section>
+      <Header />
     </main>
   )
 }
