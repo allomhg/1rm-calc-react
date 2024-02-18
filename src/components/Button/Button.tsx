fcf4670a26a5d24f6b2cde2ed06d527ba8dbc92e
allomhg <allomhg@gmail.com>
2024-02-18 18:57:57 +0000

    Adding table stuff

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,7 @@
 export const Button = () => {
     return (
-        <button type="button" className="py-2 px-8 my-4 rounded-full max-w-60 text-white bg-sky-500">Calculate 1RM</button>
+        <button type="button" className="py-2 px-8 my-4 rounded-full max-w-60 text-white bg-sky-500">
+            Calculate 1RM
+        </button>
     );
 }
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,15 +1,40 @@
 
+const data = {
+    percentage: 100,
+    weight: 50,
+    reps: 10
+}
 
 
 export const Table = () => {
+
+    const rows = [];
+
+    for (let i = 0; i < data.percentage; i += 10) {
+        rows.push(
+            <tr className="border-slate-300 border-b-2">
+                <td>{data.percentage}</td>
+                <td>{data.weight}</td>
+                <td>{data.reps}</td>
+            </tr>
+        )
+    }
+
     return (
-        <section>
-            <table>
-                <tr>
-                    <td>1RM %</td>
-                    <td>Weight</td>
-                    <td>Reps</td>
-                </tr>
+        <section className="p-4">
+            <table className="w-full border-collapse">
+                <thead className="bg-sky-300 border-slate-400 border-b-4">
+                    <tr>
+                        <td>1RM %</td>
+                        <td>Weight</td>
+                        <td>Reps</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows.map(( rows ) => (
+                        rows
+                    ))}
+                </tbody>
             </table>
         </section>
     );
