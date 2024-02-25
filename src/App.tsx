fdbbf421adf219d44c2f606aa85640d3f41a5968
allomhg <allomhg@gmail.com>
2024-02-25 16:23:13 +0000

    Adding in weight and rep calcs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import './App.css'
 import { Header } from './components/Header/Header'
 import { Form } from './components/Form/Form'
-// import { Table } from './components/Table/Table'
-// import { WeightContext, RepsContext } from './context/Context'
-// import { InputContext } from './context/Context'
-// import { useState } from 'react'
+
 
 function App() {
-  // const [input, setInput] = useState([0, 0]);
-  
   return (
     <main className="container rounded-md border-black border-solid border-2">
       <Header />
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,31 +1,35 @@
 import { InputStateType } from "../Form/Form";
 
-const data = {
-    percentage: 100,
-    weight: 50,
-    reps: 10
-}
+// function calculateWeight() {
+//     const reps = 
+// }
 
 function generateRows( inputs: InputStateType ) {
     const rows: [JSX.Element] = [<></>];
 
     const reps = inputs.inputState.reps;
     const weight = inputs.inputState.weight;
-    const percentage: number = 100;
+    let percentage: number = 100;
+    // const history = [];
 
     // const oneRepMax: number = weight * ( 1 + reps / 30 );
 
-    for (let i = 0; i < 100; i += 10) {
-        // const calculatedWeight =
+    for ( let i = 1; i <= 10; i += 1 ) {
+        let calcWeight = weight * ( 1 + reps / 30 );
+        // let calcReps = ( ( oneRepMax / weight ) -1 ) * 30;
+
+        if ( i !== 1 ) {
+            calcWeight *= percentage / 100;
+        }
 
         rows.push(
             <tr className="border-slate-400 border-b-2">
-                <td className="bg-red-200 p-1 pl-2">{ percentage - i }</td>
-                <td className="bg-red-300 p-1 pl-2">{ weight * (1 + reps / 30) }</td>
-                <td className="bg-red-400 p-1 pl-2">{ reps }</td>
+                <td className="bg-red-200 p-1 pl-2">{ percentage }</td>
+                <td className="bg-red-300 p-1 pl-2">{ calcWeight }</td>
+                <td className="bg-red-400 p-1 pl-2">{ i }</td>
             </tr>
         )
-        data.reps -= 1;
+        percentage -= 10;
     }
     return rows;
 }
