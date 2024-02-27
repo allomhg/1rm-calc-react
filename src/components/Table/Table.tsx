afd28e36402975b39d9c28c373a3e31882530d5f
allomhg <allomhg@gmail.com>
2024-02-27 21:21:20 +0000

    Changing calculation formula

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -10,25 +10,29 @@ function generateRows( inputs: InputStateType ) {
     const reps = inputs.inputState.reps;
     const weight = inputs.inputState.weight;
     let percentage: number = 100;
-    let calcWeight: number = 0;
+    let calcWeight: number = 0, calcReps: number = 0;
     // const history = [];
 
-    // const oneRepMax: number = weight * ( 1 + reps / 30 );
+    /*
+        Going to use the Brzycki formula instead to calculate 1RM and estimated repetitions
+    */
+
+    const oneRepMax: number = weight * ( 1 + reps / 30 );
 
     for ( let i = 1; i <= 10; i += 1 ) {
         if ( i !== 1 ) {
-            calcWeight *= percentage / 100;
-            // calcReps = ( ( calcWeight / weight ) -1 ) * 30;
+            calcWeight = oneRepMax * ( percentage / 100 );
+            calcReps = ( ( oneRepMax / calcWeight ) - 1 ) * 30;
         } else {
             calcWeight = weight * ( 1 + reps / 30 );
-            // calcReps = ( ( calcWeight / weight ) -1 ) * 30;
+            calcReps = ( ( oneRepMax / weight ) - 1 ) * 30;
         }
 
         rows.push(
             <tr className="border-slate-400 border-b-2">
                 <td className="bg-red-200 p-1 pl-2">{ percentage }</td>
                 <td className="bg-red-300 p-1 pl-2">{ calcWeight }</td>
-                <td className="bg-red-400 p-1 pl-2">{ i }</td>
+                <td className="bg-red-400 p-1 pl-2">{ calcReps }</td>
             </tr>
         )
         percentage -= 10;
